b383fb9cca3c8f7d32875b12333c5fb2e220b780
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-06-20 10:27:47 +0530

    UnderWork

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {Connection, IDatabaseDriver, MikroORM} from '@mikro-orm/core'
 import { prod } from './constatnts'
 import { Post }  from './enteties/post';
 import  express  from 'express'
-
+require('dotenv')
 
 async function main(){
 const app = express()
@@ -10,7 +10,7 @@ const app = express()
 const orm: MikroORM<IDatabaseDriver<Connection>> = await MikroORM.init({
  entities: [Post],
   dbName: 'Backenddb',
-  clientUrl: 'mongodb+srv://totu:aashi1234@cluster0.xibbs.mongodb.net/Data',
+  clientUrl: process.env.TOKEN ,
   debug: prod,
   type: 'mongo',
 })
