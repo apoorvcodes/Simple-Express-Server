20b755b320d436dc735e398beb13f57036c17367
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-06-21 20:44:44 +0530

    x

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,11 @@
+import  express  from 'express';
+import { Port } from './constants';
 
-import {Connection, IDatabaseDriver, MikroORM} from '@mikro-orm/core'
-import {  Port } from './constants'
-import { Post }  from './enteties/post';
-import  express  from 'express'
-import Mikroconfig from './mikro-orm.config'
-import { ApolloServer } from 'apollo-server-express';
-import {buildSchema} from 'type-graphql'
-import { Hello } from './resolvers/hello';
-import { PostResolver } from './resolvers/PostResolver';
-async function main(){
-	
-const orm: MikroORM<IDatabaseDriver<Connection>> = await MikroORM.init(Mikroconfig)
+async function Main() {
 const app = express()
 
-
-const apolloServer = new ApolloServer({
-schema: await buildSchema({
-resolvers: [Hello, PostResolver],
-validate: false
-}),
-context:  () =>({ em : orm.em})
-});
-
-
-
-apolloServer.applyMiddleware({ app });
-app.get('/', (_ ,res)=>{
-res.send('Hello World')
-})
-
-
-const Posts = await orm.em.find(Post,{})
-console.log(Posts)
-app.listen(Port, () => console.log("Starting"))
-
+app.listen(Port, () => console.log('Working'))
 }
-
-main().catch(err  => {
-console.error(err)
-});
-
+Main().catch(err  =>{
+	console.log(err)
+})
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,3 @@
-import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
-
-export interface MyContext{
-em : EntityManager<IDatabaseDriver<Connection>>
+export interface Main{
+	
 }
