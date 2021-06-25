ea7563a7a60552448f036aef589ca7d0ca9fc117
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-06-25 08:36:38 +0530

    s

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 
 export const prod = process.env.NODE_ENV === 'production'
-export const Port = 3000 
+export const Port = process.env.PORT || 3000 
 export const MongoDBPass = "mongodb+srv://totu:aashi1234@cluster0.xibbs.mongodb.net/Data" ;
 export const Host = 'localhost'
 
